8b068b8b818a720308384b044ecbb1d7126fc725
Emmanuel Najfar <if20b200@technikum-wien.at>
2021-04-12 20:09:24 +0200

    Refactor Code and write Unit Tests

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -7,35 +7,30 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
-                    item.sellIn -= 1;
-                    item.quality += 1;
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (item.sellIn < 1) {
-                        item.quality = 0;
-                    } else if (item.sellIn < 6) {
-                        item.quality += 3;
-                    } else if (item.sellIn < 11) {
-                        item.quality += 2;
-                    } else {
-                        item.quality += 1;
-                    }
-                    item.sellIn -= 1;
-                    break;
-                case "Conjured Mana Cake":
-                    item.sellIn -= 1;
-                    item.quality -= 2;
-                    break;
-                default:
-                    item.sellIn -= 1;
-                    item.quality -= 1;
-                    break;
-            }
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn -= 1;
+                switch (item.name) {
+                    case "Aged Brie":
+                        item.quality += 1;
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        if (item.sellIn < 1) {
+                            item.quality = 0;
+                        } else if (item.sellIn < 6) {
+                            item.quality += 3;
+                        } else if (item.sellIn < 11) {
+                            item.quality += 2;
+                        } else {
+                            item.quality += 1;
+                        }
+                        break;
+                    case "Conjured Mana Cake":
+                        item.quality -= 2;
+                        break;
+                    default:
+                        item.quality -= 1;
+                        break;
+                }
                 if (item.sellIn < 1) {
                     if (item.name.equals("Aged Brie")) {
                         item.quality += 1;
@@ -51,64 +46,5 @@ class GildedRose {
             }
         }
     }
-
-    /*
-    public void updateQuality() {
-        for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.name.equals("Conjured Mana Cake")) {
-                item.quality = item.quality - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
-        }
-    }
-    */
 
 }
